fa1e897ed0c611dbcebf346de267be48e52b622a
joesabbagh1 <joe.sabbagh2001@gmail.com>
2024-07-07 23:57:32 +0300

    feat: set up github workspace

--- locals.tf
+++ locals.tf
@@ -12,5 +12,12 @@ locals {
       project_id          = module.project["fem-eci-project"].id
       vsc_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
+
+    "fem-eci-github" = {
+      description         = "Automation for GitHub"
+      execution_mode      = "local"
+      project_id          = module.project["fem-eci-project"].id
+      vsc_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
+    }
   }
 }
