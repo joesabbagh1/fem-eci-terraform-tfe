82d9ababbfca2c16d1aa2775c4316c76f058dc48
joesabbagh1 <joe.sabbagh2001@gmail.com>
2024-07-08 18:32:47 +0300

    add workspaces

--- locals.tf
+++ locals.tf
@@ -10,14 +10,70 @@ locals {
       description         = "example workspace"
       execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
-      vsc_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
 
     "fem-eci-github" = {
       description         = "Automation for GitHub"
       execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
-      vsc_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
+    }
+
+    "fem-eci-aws-network" = {
+      description         = "Automation for AWS network resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-network"
+
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "azs"
+          value    = jsonencode(["us-west-2a", "us-west-2b"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci"
+        },
+      ]
+    }
+
+    "fem-eci-aws-cluster" = {
+      description         = "Automation for AWS cluster resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-cluster"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "project.dev"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci-joe"
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "fem-eci"
+        },
+      ]
     }
   }
 }
